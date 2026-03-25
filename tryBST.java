5542a15a2442f0c955a10782013be8ef43bc1993
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-25 15:00:12 +0200

    public insert method

--- tryBST.java
+++ tryBST.java
@@ -11,3 +11,19 @@ class tNode{
     this.right = null;
   }
 }
+class BST {
+  tNode root;
+
+  BST(){
+    root = null;
+  }
+  tNode insert(tNode node, int data){
+    if (node == null)
+      return new tNode(data);
+    if (data <= node.data)
+      node.left = insert(node.left, data);
+    else if (data > node.data)
+      node.right = insert(node.right, data);
+    return node;
+  }
+}
