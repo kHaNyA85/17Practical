5840ec51d06c5add51ab1118d5e6c1ed9b1356c9
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-25 14:48:45 +0200

    Binary Search Tree Implementation

--- tryBST.java
+++ tryBST.java
@@ -1,1 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+class tNode{
+  int data;
+  tNode left, right;
+
+  tNode(int data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
